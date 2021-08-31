b151d96a1d5fff972addc2a79c6197086b05d1ee
NGUYEN HAI NAM <sepnamdz@gmail.com>
2021-08-31 14:51:24 +0700

    baitapgiaipt

--- baitap3/baitap3/Controllers/HomeController.cs
+++ baitap3/baitap3/Controllers/HomeController.cs
@@ -3,28 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using baitap3.Models;
 
 namespace baitap3.Controllers
 {
     public class HomeController : Controller
     {
+        GiaiPhuongTrinh gpt = new GiaiPhuongTrinh();
+        // GET: DeMo
         public ActionResult Index()
+
         {
+
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult GiaiPT(string SoA, string SoB)
+        {
+            double a = double.Parse(SoA);
+            double b = double.Parse(SoB);
+            double x = gpt.GiaiPT(a, b);
+            ViewBag.NghiemPT = x;
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ baitap3/baitap3/Models/GiaiPhuongTrinhController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace baitap3.Models
+{
+    public class GiaiPhuongTrinh
+    {
+        public double GiaiPT(double a, double b)
+        {
+            double x = 0;
+            x = -b / a;
+            return x;
+        }
+    }
+}
